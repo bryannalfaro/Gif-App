2bbf095ed1b60a1ab28ac38c187e21408d28e404
Bryann <alf19372@uvg.edu.gt>
2024-02-12 15:13:59 -0600

    ADD archivos de barril y isLoading logic

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -7,22 +7,12 @@ export const GifGrid = ({ category }) => {
 
     const { images, isLoading } = useFetchGifs(category)
 
-    // const [images, setImages ] = useState([])
-
-    // const getImages = async() => {   
-    //         const newImages = await getGifs(category)
-    //         setImages(newImages)
-    // }
-
-    // useEffect(()=>{
-        
-    //     getImages()
-    // }, [])
-
-
     return(
             <>
                 <h3>{category}</h3>
+                {
+                    isLoading && (<h2>Loading...</h2>)
+                }
                 <div className="card-grid">
                     {
                         images.map(image => {
